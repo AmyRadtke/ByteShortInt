f682cdf21119e61adec9c7bf108a20666970e783
Amy Radtke <amy.freelance.dev@gmail.com>
2016-11-01 11:16:15 -0400

    create new array values

--- src/com/javacourse/ArrayTest.java
+++ src/com/javacourse/ArrayTest.java
@@ -4,4 +4,12 @@ package com.javacourse;
  * Created by Mom and Dad on 11/1/2016.
  */
 public class ArrayTest {
+
+   public static void main(String[] args){
+       int[] marks = {1, 2, 4, 5, 6};
+       for (int i: marks) {
+           System.out.print(i + " ");
+       }
+   }
+
 }
